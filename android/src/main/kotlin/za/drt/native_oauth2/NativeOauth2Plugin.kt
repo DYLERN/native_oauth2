1523ebf3970f6d9073b16685ff50befa81faf7d4
Dylan Pfab <dylan@squarepeg.co.za>
2023-03-15 16:05:24 +0200

    Added android implementation

--- android/src/main/kotlin/za/drt/native_oauth2/NativeOauth2Plugin.kt
+++ android/src/main/kotlin/za/drt/native_oauth2/NativeOauth2Plugin.kt
@@ -1,35 +1,120 @@
 package za.drt.native_oauth2
 
-import androidx.annotation.NonNull
-
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.browser.customtabs.CustomTabsIntent
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry
+import java.util.*
+import kotlin.concurrent.schedule
 
 /** NativeOauth2Plugin */
-class NativeOauth2Plugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class NativeOauth2Plugin : FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegistry.NewIntentListener, PluginRegistry.ActivityResultListener {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private lateinit var activity: Activity
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "native_oauth2")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    companion object {
+        const val CHROME_CUSTOM_TABS_REQUEST_CODE = 100
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    private var redirectListener: ((redirectUrl: String) -> Unit)? = null
+    private var cancelListener: (() -> Unit)? = null
+
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "za.drt/native_oauth2")
+        channel.setMethodCallHandler(this)
+
+    }
+
+    override fun onMethodCall(call: MethodCall, result: Result) {
+        when (call.method) {
+            "authenticate" -> authenticate(call, result)
+            else -> result.error(
+                    "UNSUPPORTED_OPERATION",
+                    "The ${call.method} operation is not supported on Android",
+                    null
+            )
+        }
+    }
+
+    private fun authenticate(call: MethodCall, result: Result) {
+        val launchUri = call.argument<String>("authUri")
+
+        if (launchUri == null) {
+            result.error("MISSING_ARGUMENT", "launchUri argument is required", null)
+            return
+        }
+
+        redirectListener = { redirectUrl ->
+            result.success(redirectUrl)
+            cancelListener = null
+        }
+        cancelListener = {
+            result.success(null)
+            redirectListener = null
+        }
+
+        displayCustomTab(launchUri)
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onNewIntent(intent: Intent): Boolean {
+        val redirectUri = intent.dataString
+        if (redirectUri != null) {
+            redirectListener?.invoke(redirectUri)
+            return true
+        }
+        return false
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
+        return when (requestCode) {
+            CHROME_CUSTOM_TABS_REQUEST_CODE -> {
+                Timer().schedule(1000) {
+                    cancelListener?.invoke()
+                }
+
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun displayCustomTab(uriString: String) {
+        val intent = CustomTabsIntent.Builder().build()
+        intent.intent.data = Uri.parse(uriString)
+        activity.startActivityForResult(intent.intent, CHROME_CUSTOM_TABS_REQUEST_CODE)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity
+        binding.addOnNewIntentListener(this)
+        binding.addActivityResultListener(this)
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        // Not implemented
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        // Not implemented
+    }
+
+    override fun onDetachedFromActivity() {
+        // Not implemented
+    }
 }
